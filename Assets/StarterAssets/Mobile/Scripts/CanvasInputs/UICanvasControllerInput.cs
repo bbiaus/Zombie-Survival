980ee81ec437f68af28d95c4fe0d21afd09b1a53
bbiaus <biaus.boris@gmail.com>
2025-03-20 22:36:28 -0300

    Modificando el player para ver si ahora funciona

--- Assets/StarterAssets/Mobile/Scripts/CanvasInputs/UICanvasControllerInput.cs
+++ Assets/StarterAssets/Mobile/Scripts/CanvasInputs/UICanvasControllerInput.cs
@@ -16,7 +16,6 @@ namespace StarterAssets
         public void VirtualLookInput(Vector2 virtualLookDirection)
         {
             starterAssetsInputs.LookInput(virtualLookDirection);
-
         }
 
         public void VirtualJumpInput(bool virtualJumpState)
