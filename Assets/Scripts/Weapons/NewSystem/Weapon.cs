9b8a10b1cbdedfb1ed1d00eedacef02da2d44da9
Lauta55 <lautaromiguelsolis@gmail.com>
2025-04-16 22:09:37 -0300

    agrego escena para testear. +Rifle de asalto nuevo +VFX +sonido de cargador vacio

--- Assets/Scripts/Weapons/NewSystem/Weapon.cs
+++ Assets/Scripts/Weapons/NewSystem/Weapon.cs
@@ -1,20 +1,22 @@
 using UnityEngine;
 using Weapons.NewSystem.Data;
 using StarterAssets;
+using System.Collections;
 
 namespace WeaponSystem
 {
     public class Weapon : MonoBehaviour
     {
-        [SerializeField] private Transform _firepoint; // Punto de aparición de la bala
+        [SerializeField] private Transform _firePoint; // Punto de aparición de la bala
         [SerializeField] private AudioSource _audioSource; // Sonido del disparo
+        [SerializeField] private AudioSource _outOfAmmoAudioSource; // Sonido de recarga
         [SerializeField] private ParticleSystem _muzzleFlash; // Efecto de destello al disparar
         private WeaponData _weaponData;
 
         
 
 
-        public Transform FirePoint => _firepoint; //get de firepoint
+        public Transform FirePoint => _firePoint; //get de firepoint
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
@@ -38,11 +40,43 @@ namespace WeaponSystem
                 Bullet bullet = BulletPool.Instance.GetBullet(); //saco una bala del pool
                 if (bullet == null) return;
 
-                bullet.Shoot(_firepoint.position, Quaternion.LookRotation(shootDirection));
+                // Instanciamos una copia del prefab del muzzleflash en la posición del fire point
+                ParticleSystem flash = Instantiate(_muzzleFlash, _firePoint.position, _firePoint.rotation, _firePoint);
+
+
+                // se le aplica una rotación aleatoria en Z para que se vea distinto
+                float random = Random.Range(0f, 360f);
+                flash.transform.Rotate(0f, 0f, random);
+
+                bullet.Shoot(_firePoint.position, Quaternion.LookRotation(shootDirection));
                  // Activa el efecto de disparo
-                if (_muzzleFlash != null) _muzzleFlash.Play();
+                if (_muzzleFlash != null)
+                {
+                    flash.Play(); // Reproducir el efecto de destello
+                    // Se destruye después de un rato (ya que es una copia)
+                    Destroy(flash.gameObject, 0.3f); // Destruye el efecto después de poco tiempo
+                } 
+
                 //// Activa el sonido de disparo
                 if (_weaponData.ShootSound != null && _audioSource != null) _audioSource.PlayOneShot(_weaponData.ShootSound, 0.15f);
             }
+
+        private bool _canPlayNoAmmoSound = true; // cooldown para el sonido de gatillo, (arma vacia)
+
+        public void noAmmoSound()
+        {
+            if (_canPlayNoAmmoSound && _outOfAmmoAudioSource != null && _outOfAmmoAudioSource.clip != null)
+            {
+                _outOfAmmoAudioSource.PlayOneShot(_outOfAmmoAudioSource.clip, 0.5f);
+                StartCoroutine(NoAmmoCooldownCoroutine());
+            }
+        }
+
+        private IEnumerator NoAmmoCooldownCoroutine()
+        {
+            _canPlayNoAmmoSound = false;
+            yield return new WaitForSeconds(0.2f); // cooldown
+            _canPlayNoAmmoSound = true;
+        }
     }
 }
