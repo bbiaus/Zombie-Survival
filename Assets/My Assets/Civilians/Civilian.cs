9b6cdebd2e16ddb55925309b3f7ddeb8de78e4c0
bbiaus <biaus.boris@gmail.com>
2025-04-24 02:32:45 -0300

    +Anim a civiles +SFX +Edite un poco el mapa

--- Assets/My Assets/Civilians/Civilian.cs
+++ Assets/My Assets/Civilians/Civilian.cs
@@ -7,12 +7,17 @@ public class Civilian : MonoBehaviour
     private Transform player;
     private NavMeshAgent agent;
     private bool isFollowing = false;
+    private AudioSource audioSource;
+    private Animator animator;
+    private bool isIdle = true;
 
     private float deathTimer = 60f; // Tiempo límite para salvarlo
 
     private void Start()
     {
+        animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
+        audioSource = GetComponent<AudioSource>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
         StartCoroutine(DeathCountdown());
     }
@@ -22,6 +27,21 @@ public class Civilian : MonoBehaviour
         if (isFollowing)
         {
             agent.SetDestination(player.position);
+            // Actualiza el parámetro de velocidad
+            float speed = agent.velocity.magnitude;
+            animator.SetFloat("Speed", speed);
+
+            if (speed < 0.1f && !isIdle)
+            {
+                // Randomiza una de las dos animaciones cuando estan quietos
+                int idleChoice = Random.Range(0, 2); // 0 o 1
+                animator.SetInteger("idleIndex", idleChoice);
+                isIdle = true;
+            }
+            else if (speed >= 0.1f)
+            {
+                isIdle = false;
+            }
         }
     }
 
@@ -30,7 +50,9 @@ public class Civilian : MonoBehaviour
         if (other.CompareTag("Player") && !isFollowing)
         {
             isFollowing = true;
-            agent.speed = 3.5f;
+            audioSource.Stop(); // Detenemos el grito de ayuda
+            animator.SetBool("isFollowing", true); // Animación de correr
+            agent.speed = 5.5f;
         }
         else if (other.CompareTag("Shelter") && isFollowing)
         {
@@ -42,8 +64,8 @@ public class Civilian : MonoBehaviour
     private IEnumerator DeathCountdown()
     {
         yield return new WaitForSeconds(deathTimer);
-        
-        if (!isFollowing) 
+
+        if (!isFollowing)
         {
             Destroy(gameObject);
         }
